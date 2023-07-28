6c289fd18bce074ab85abad249c9bb658b2334e5
svartse <killbillbeatrixx@gmail.com>
2023-07-28 16:06:23 +0300

    register

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,3 +1,24 @@
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import User from "../models/User.js";
+
+/* register user */
+
+export const register = async (req, res) => {
+    try{
+        const{
+            firstName,
+            lastName,
+            email,
+            password,
+            picturePath,
+            friends,
+            location,
+            occupation 
+        } = req.body;
+
+        const salt = await bcrypt.genSalt();
+    }catch(err){
+
+    }
+}
