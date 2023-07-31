b77fee68e1a1e3abc10da52afc777e7e52d3289c
svartse <killbillbeatrixx@gmail.com>
2023-07-31 15:10:26 +0300

    authentication

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath  } from "url";
+import authRoutes from "./routes.auth.js";
 import { register } from "/controllers/auth.js";
 
 /* configurations */
@@ -38,6 +39,9 @@ const upload = multer ({ storage });
 
 /* Routes with files */
 app.post("/auth/register", upload.single("picture"), register);
+
+/* Routes */
+app.use("/auth", authRoutes);
 
 /* mongoose setup */
 
